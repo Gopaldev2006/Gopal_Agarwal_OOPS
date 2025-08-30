df965de7cdadb40baaf5ab546c33fc55a6082bbf
Gopaldev2006 <gplsinghaniya@gmail.com>
2025-08-30 15:27:25 +0530

    Add new files

---
+++ Shift.java
@@ -0,0 +1,7 @@
+public class Shift{
+    public static void main(String[]args){
+        int a =64928;
+        int b =8;
+        System.out.println(a>>b);
+    }
+}
---
+++ operator.java
@@ -0,0 +1,19 @@
+//write a java program to calculate size and range on primitive data types//
+//write a java program to check even or odd without using modlus operator //
+import java.util.*;
+public class primitive {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int sum = a + b;
+        System.out.println("The Sum is: "+sum);
+        int sub=a-b;
+        System.out.println("the sub is :"+sub);
+        int mul=a*b;
+        System.out.println("the mul is :"+mul);
+        int div=a/b;
+        System.out.println("the div is :"+div);
+
+    }
+
+    }
