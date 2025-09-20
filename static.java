10f024ee10e8f716cb67b99f9c3fa529e168104d
Gopaldev2006 <gplsinghaniya@gmail.com>
2025-09-20 14:35:09 +0530

    intially

---
+++ static.java
@@ -0,0 +1,10 @@
+class main{
+    static {
+        System.out.println("Hello, World!");
+    }
+    public  static void main(String[] args) {
+    {
+        System.out.println("from main");
+    }
+}
+}
